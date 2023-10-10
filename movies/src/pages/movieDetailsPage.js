4df81acaeddaad9c11577267aea7bf47036e3f75
unknown <20088394@mail.wit.ie>
2023-10-10 22:50:20 +0100

    Movie Details coming from API.

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -1,4 +1,6 @@
-import React from "react";
+// import React from "react";
+import React, {useState, useEffect}  from "react";
+import { useParams } from 'react-router-dom';
 import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
 import Grid from "@mui/material/Grid";
@@ -6,8 +8,39 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
 const MoviePage = (props) => {
-  const movie = props.movie;
-  const images = props.images;
+  
+  // const movie = props.movie;
+  // const images = props.images;
+
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((movie) => {
+        // console.log(movie)
+        setMovie(movie);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters)
+      .then((images) => {
+        // console.log(images)
+        setImages(images);
+      });
+      // eslint-disable-next-line
+  }, []);
 
   return (
     <>
@@ -21,7 +54,8 @@ const MoviePage = (props) => {
                 flexWrap: "wrap",
                 justifyContent: "space-around",
               }}>
-                <ImageList
+
+                {/* <ImageList
                   cellHeight={500}
                   sx={{
                     height: "100vh",
@@ -36,6 +70,26 @@ const MoviePage = (props) => {
                       <img
                         src={`https://image.tmdb.org/t/p/w500/${image}`}
                         alt={image.poster_path}
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList> */}
+
+              <ImageList
+                  cellHeight={500}
+                  sx={{
+                    height: "100vh",
+                  }}
+                  cols={1}
+                >
+                  {images.map((image) => (
+                    <ImageListItem
+                    key={image.file_path}
+                    cols={1}
+                    >
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                        alt={image.file_path}
                       />
                     </ImageListItem>
                   ))}
