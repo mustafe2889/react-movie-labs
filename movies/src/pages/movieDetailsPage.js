d9469cce7420d890d44f3bdfa77f738f473c501e
unknown <20088394@mail.wit.ie>
2023-10-12 20:41:47 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -6,6 +6,8 @@ import MovieDetails from "../components/movieDetails/";
 import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import { getMovie, getMovieImages } from "../api/tmdb-api";
+
 
 const MoviePage = (props) => {
   
@@ -16,30 +18,43 @@ const MoviePage = (props) => {
   const [movie, setMovie] = useState(null);
   const [images, setImages] = useState([]);
 
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  //   )
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((movie) => {
+  //       // console.log(movie)
+  //       setMovie(movie);
+  //     });
+  // }, [id]);
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => json.posters)
+  //     .then((images) => {
+  //       // console.log(images)
+  //       setImages(images);
+  //     });
+  //     // eslint-disable-next-line
+  // }, []);
+
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((movie) => {
-        // console.log(movie)
-        setMovie(movie);
-      });
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
   }, [id]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters)
-      .then((images) => {
-        // console.log(images)
-        setImages(images);
-      });
-      // eslint-disable-next-line
+    getMovieImages(id).then((images) => {
+      setImages(images);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
