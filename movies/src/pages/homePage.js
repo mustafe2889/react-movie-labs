07d1e30d2e5f1657da94f9fa12e17d806930d5d4
unknown <20088394@mail.wit.ie>
2023-10-12 20:35:46 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -25,6 +25,13 @@ const MovieListPage = (props) => {
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -59,7 +66,11 @@ const MovieListPage = (props) => {
 
 
         {/* <MovieList movies={movies}></MovieList> */}
-        <MovieList movies={displayedMovies} />
+
+        {/* <MovieList movies={displayedMovies} /> */}
+
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+
 
       
       </Grid>
