8a9c4af784871543f47ab0a3393e92ec7c121d71
unknown <20088394@mail.wit.ie>
2023-10-18 16:35:22 +0000

    Show movie review excerpts; Refactor Details page.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -7,6 +7,8 @@ import StarRate from "@mui/icons-material/StarRate";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews"
 
 
 const root = {
@@ -19,8 +21,11 @@ const root = {
 };
 const chip = { margin: 0.5 };
 
-const MovieDetails = ( props) => {
-  const movie = props.movie
+// const MovieDetails = ( props) => {
+//   const movie = props.movie
+
+const MovieDetails = ({ movie }) => {  // Don't miss this!
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
@@ -73,7 +78,7 @@ const MovieDetails = ( props) => {
           </li>
         ))}
       </Paper>
-      <Fab
+      {/* <Fab
         color="secondary"
         variant="extended"
         sx={{
@@ -84,7 +89,24 @@ const MovieDetails = ( props) => {
       >
         <NavigationIcon />
         Reviews
+      </Fab> */}
+
+        <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() =>setDrawerOpen(true)}
+        sx={{
+          position: 'fixed',
+          bottom: '1em',
+          right: '1em'
+        }}
+      >
+        <NavigationIcon />
+        Reviews
       </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
       </>
   );
 };
--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -1,48 +1,141 @@
-// import React from "react";
+// // import React from "react";
+// import React, {useState, useEffect}  from "react";
+// import { useParams } from 'react-router-dom';
+// import MovieHeader from "../components/headerMovie/";
+// import MovieDetails from "../components/movieDetails/";
+// import Grid from "@mui/material/Grid";
+// import ImageList from "@mui/material/ImageList";
+// import ImageListItem from "@mui/material/ImageListItem";
+// import { getMovie, getMovieImages } from "../api/tmdb-api";
+
+
+// const MoviePage = (props) => {
+  
+//   // const movie = props.movie;
+//   // const images = props.images;
+
+//   const { id } = useParams();
+//   const [movie, setMovie] = useState(null);
+//   const [images, setImages] = useState([]);
+
+//   // useEffect(() => {
+//   //   fetch(
+//   //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+//   //   )
+//   //     .then((res) => {
+//   //       return res.json();
+//   //     })
+//   //     .then((movie) => {
+//   //       // console.log(movie)
+//   //       setMovie(movie);
+//   //     });
+//   // }, [id]);
+
+//   // useEffect(() => {
+//   //   fetch(
+//   //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+//   //   )
+//   //     .then((res) => res.json())
+//   //     .then((json) => json.posters)
+//   //     .then((images) => {
+//   //       // console.log(images)
+//   //       setImages(images);
+//   //     });
+//   //     // eslint-disable-next-line
+//   // }, []);
+
+//   useEffect(() => {
+//     getMovie(id).then((movie) => {
+//       setMovie(movie);
+//     });
+//   }, [id]);
+
+//   useEffect(() => {
+//     getMovieImages(id).then((images) => {
+//       setImages(images);
+//     });
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+
+//   return (
+//     <>
+//       {movie ? (
+//         <>
+//           <MovieHeader movie={movie} />
+//           <Grid container spacing={5} style={{ padding: "15px" }}>
+//             <Grid item xs={3}>
+//               <div sx={{
+//                 display: "flex",
+//                 flexWrap: "wrap",
+//                 justifyContent: "space-around",
+//               }}>
+
+//                 {/* <ImageList
+//                   cellHeight={500}
+//                   sx={{
+//                     height: "100vh",
+//                   }}
+//                   cols={1}
+//                 >
+//                   {images.map((image) => (
+//                     <ImageListItem
+//                       key={image.file_path}
+//                       cols={1}
+//                     >
+//                       <img
+//                         src={`https://image.tmdb.org/t/p/w500/${image}`}
+//                         alt={image.poster_path}
+//                       />
+//                     </ImageListItem>
+//                   ))}
+//                 </ImageList> */}
+
+//               <ImageList
+//                   cellHeight={500}
+//                   sx={{
+//                     height: "100vh",
+//                   }}
+//                   cols={1}
+//                 >
+//                   {images.map((image) => (
+//                     <ImageListItem
+//                     key={image.file_path}
+//                     cols={1}
+//                     >
+//                       <img
+//                         src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+//                         alt={image.file_path}
+//                       />
+//                     </ImageListItem>
+//                   ))}
+//                 </ImageList>
+//               </div>
+//             </Grid>
+//             <Grid item xs={9}>
+//               <MovieDetails movie={movie} />
+//             </Grid>
+//           </Grid>
+//         </>
+//       ) : (
+//         <h2>Waiting for API data</h2>
+//       )}
+//     </>
+//   );
+// };
+
+// export default MoviePage;
+
+
+
 import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom';
-import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
-import Grid from "@mui/material/Grid";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
-
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
 const MoviePage = (props) => {
-  
-  // const movie = props.movie;
-  // const images = props.images;
-
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-  //   )
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((movie) => {
-  //       // console.log(movie)
-  //       setMovie(movie);
-  //     });
-  // }, [id]);
-
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((json) => json.posters)
-  //     .then((images) => {
-  //       // console.log(images)
-  //       setImages(images);
-  //     });
-  //     // eslint-disable-next-line
-  // }, []);
 
   useEffect(() => {
     getMovie(id).then((movie) => {
@@ -50,74 +143,16 @@ const MoviePage = (props) => {
     });
   }, [id]);
 
-  useEffect(() => {
-    getMovieImages(id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       {movie ? (
         <>
-          <MovieHeader movie={movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
-              }}>
-
-                {/* <ImageList
-                  cellHeight={500}
-                  sx={{
-                    height: "100vh",
-                  }}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <ImageListItem
-                      key={image.file_path}
-                      cols={1}
-                    >
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500/${image}`}
-                        alt={image.poster_path}
-                      />
-                    </ImageListItem>
-                  ))}
-                </ImageList> */}
-
-              <ImageList
-                  cellHeight={500}
-                  sx={{
-                    height: "100vh",
-                  }}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <ImageListItem
-                    key={image.file_path}
-                    cols={1}
-                    >
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={image.file_path}
-                      />
-                    </ImageListItem>
-                  ))}
-                </ImageList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <MovieDetails movie={movie} />
-            </Grid>
-          </Grid>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
         </>
       ) : (
-        <h2>Waiting for API data</h2>
+        <p>Waiting for movie details</p>
       )}
     </>
   );
